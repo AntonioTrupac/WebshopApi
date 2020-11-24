8f6dbdcc43aa5276eb384084743da87eacc1a85b
AntonioTrupac <sniper.trupac@gmail.com>
2020-11-24 20:50:42 +0100

    #Models/DbContext scaffold fix

---
+++ WebShop/ShopOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebShop
+{
+    public partial class ShopOrder
+    {
+        public ShopOrder()
+        {
+            OrderItem = new HashSet<OrderItem>();
+            Shipment = new HashSet<Shipment>();
+        }
+
+        public int OrderId { get; set; }
+        public int SellerId { get; set; }
+        public string OrderDetails { get; set; }
+        public DateTime DateOrderPlaced { get; set; }
+
+        public virtual AppUser Seller { get; set; }
+        public virtual ICollection<OrderItem> OrderItem { get; set; }
+        public virtual ICollection<Shipment> Shipment { get; set; }
+    }
+}
