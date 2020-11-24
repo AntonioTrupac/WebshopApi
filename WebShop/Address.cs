699e4ad031ae5357b14506da5b0b70a9ebe636d0
AntonioTrupac <sniper.trupac@gmail.com>
2020-11-24 20:50:21 +0100

    #Models/DbContext scaffold

---
+++ WebShop/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebShop
+{
+    public partial class Address
+    {
+        public Address()
+        {
+            AppUser = new HashSet<AppUser>();
+        }
+
+        public int AddressId { get; set; }
+        public int CountryId { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+
+        public virtual Country Country { get; set; }
+        public virtual ICollection<AppUser> AppUser { get; set; }
+    }
+}
---
+++ WebShop/postgresContext.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebShop
+{
+    public partial class postgresContext : DbContext
+    {
+        public postgresContext()
+        {
+        }
+
+        public postgresContext(DbContextOptions<postgresContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Address> Address { get; set; }
+        public virtual DbSet<AppUser> AppUser { get; set; }
+        public virtual DbSet<Country> Country { get; set; }
+        public virtual DbSet<OrderItem> OrderItem { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<ProductType> ProductType { get; set; }
+        public virtual DbSet<Shipment> Shipment { get; set; }
+        public virtual DbSet<ShipmentItem> ShipmentItem { get; set; }
+        public virtual DbSet<ShopOrder> ShopOrder { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=postgres;User Id=Antonio;Password=pass;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.ToTable("address");
+
+                entity.Property(e => e.AddressId)
+                    .HasColumnName("address_id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasColumnName("city")
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.CountryId).HasColumnName("country_id");
+
+                entity.Property(e => e.PostalCode)
+                    .IsRequired()
+                    .HasColumnName("postal_code")
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Street)
+                    .IsRequired()
+                    .HasColumnName("street")
+                    .HasMaxLength(100);
+
+                entity.HasOne(d => d.Country)
+                    .WithMany(p => p.Address)
+                    .HasForeignKey(d => d.CountryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_country");
+            });
+
+            modelBuilder.Entity<AppUser>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("app_user_pkey");
+
+                entity.ToTable("app_user");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("user_id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.AddressId).HasColumnName("address_id");
+
+                entity.Property(e => e.DateOfBirth)
+                    .HasColumnName("date_of_birth")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("first_name")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("last_name")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Phone)
+                    .HasColumnName("phone")
+                    .HasMaxLength(15)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasColumnName("username")
+                    .HasMaxLength(255);
+
+                entity.HasOne(d => d.Address)
+                    .WithMany(p => p.AppUser)
+                    .HasForeignKey(d => d.AddressId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_address");
+            });
+
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.ToTable("country");
+
+                entity.Property(e => e.CountryId)
+                    .HasColumnName("country_id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.ContinentName)
+                    .IsRequired()
+                    .HasColumnName("continent_name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(74);
+            });
+
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.ToTable("order_item");
+
+                entity.Property(e => e.OrderItemId)
+                    .HasColumnName("order_item_id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderItem)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_order");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderItem)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_product");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("product");
+
+                entity.Property(e => e.ProductId)
+                    .HasColumnName("product_id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasColumnName("description")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Price)
+                    .IsRequired()
+                    .HasColumnName("price")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.ProductTypeId).HasColumnName("product_type_id");
+
+                entity.Property(e => e.Size)
+                    .IsRequired()
+                    .HasColumnName("size")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Stock).HasColumnName("stock");
+
+                entity.HasOne(d => d.ProductType)
+                    .WithMany(p => p.Product)
+                    .HasForeignKey(d => d.ProductTypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_product_type");
+            });
+
+            modelBuilder.Entity<ProductType>(entity =>
+            {
+                entity.ToTable("product_type");
+
+                entity.Property(e => e.ProductTypeId)
+                    .HasColumnName("product_type_id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(255);
+            });
+
+            modelBuilder.Entity<Shipment>(entity =>
+            {
+                entity.ToTable("shipment");
+
+                entity.Property(e => e.ShipmentId)
+                    .HasColumnName("shipment_id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.ShipmentDate)
+                    .HasColumnName("shipment_date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.ShipmentTrackingNumber).HasColumnName("shipment_tracking_number");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.Shipment)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_seller");
+            });
+
+            modelBuilder.Entity<ShipmentItem>(entity =>
+            {
+                entity.HasKey(e => new { e.ShipmentId, e.OrderItemId })
+                    .HasName("shipment_item_pkey");
+
+                entity.ToTable("shipment_item");
+
+                entity.Property(e => e.ShipmentId).HasColumnName("shipment_id");
+
+                entity.Property(e => e.OrderItemId).HasColumnName("order_item_id");
+
+                entity.HasOne(d => d.OrderItem)
+                    .WithMany(p => p.ShipmentItem)
+                    .HasForeignKey(d => d.OrderItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("shipment_item_order_item_id_fkey");
+
+                entity.HasOne(d => d.Shipment)
+                    .WithMany(p => p.ShipmentItem)
+                    .HasForeignKey(d => d.ShipmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("shipment_item_shipment_id_fkey");
+            });
+
+            modelBuilder.Entity<ShopOrder>(entity =>
+            {
+                entity.HasKey(e => e.OrderId)
+                    .HasName("shop_order_pkey");
+
+                entity.ToTable("shop_order");
+
+                entity.Property(e => e.OrderId)
+                    .HasColumnName("order_id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.DateOrderPlaced)
+                    .HasColumnName("date_order_placed")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.OrderDetails)
+                    .HasColumnName("order_details")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.SellerId).HasColumnName("seller_id");
+
+                entity.HasOne(d => d.Seller)
+                    .WithMany(p => p.ShopOrder)
+                    .HasForeignKey(d => d.SellerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_seller");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
